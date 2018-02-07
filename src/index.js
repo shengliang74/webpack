d152e76aa37c0714a2b3e54a110d06374934eda3
shengliang74 <shengliang74@yeah.net>
2018-02-07 22:33:24 +0800

    第三次提交

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import _ from 'lodash';
+import printMe from './print.js';
+
+function component() {
+	var element = document.createElement('div');
+	var btn = document.createElement('button');
+
+	element.innerHTML = _.join(['shengliang', 'SingleMalta'], ' ');
+	btn.innerHTML = 'Click me and check the console!';
+	btn.onclick = printMe;
+
+	element.appendChild(btn);
+	return element;
+}
+
+document.body.appendChild(component());
