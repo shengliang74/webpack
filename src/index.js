c821e1e212649621982e8355a96fbf41e4dea297
sl46465 <sl46465@ly.com>
2018-02-09 10:54:15 +0800

    热替换

--- src/index.js
+++ src/index.js
@@ -1,28 +1,13 @@
 import _ from 'lodash';
-import printMe from './print.js';
-import './styles.css';
 
-function component() {
-	var element = document.createElement('div');
-	var btn = document.createElement('button');
-
-	element.innerHTML = _.join(['shengliang', 'SingleMalta'], ' ');
-	btn.innerHTML = 'Click me and check the console!';
-	btn.onclick = printMe;
-
-	element.appendChild(btn);
-	return element;
+function getComponent() {
+	return import(/* webpackChunkName: "lodash" */ 'lodash').then( _ => {
+		var element = document.createElement('div');
+		element.innerHTML = _.join(['Hello','webpack',' ']);
+		return element;
+	}).catch(error => 'An error occurred while loading the component')
 }
 
-let element = component();
-document.body.appendChild(element);
-
-if (module.hot) {
-   module.hot.accept('./print.js', function() {
-     console.log('Accepting the updated printMe module!');
-     printMe();
-     document.body.removeChild(element);
-     element = component();
-     document.body.appendChild(element);
-   })
-}
+getComponent().then(component => {
+	document.body.appendChild(component);
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,18 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: { 
-		'app': './src/index.js',
-	},
-	output: {
-		filename: '[name].bundle.js',
-		path: path.resolve(__dirname, 'dist'),
-		publicPath: '/',
-	},
-	devtool: 'inline-source-map',
-	devServer: {
-		contentBase: './dist',
-		hot: true
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ['style-loader','css-loader']
-			}
-		]
+	entry: {
+		index: './src/index.js',
 	},
 	plugins: [
-		new CleanWebpackPlugin(['dist']),
-		new HtmlWebpackPlugin({
-			title: 'Output Management'
-		}),
-		new webpack.NamedModulesPlugin(),
-		new webpack.HotModuleReplacementPlugin()
+		new HTMLWebpackPlugin({
+			title: 'Code Splitting'
+		})
 	],
+	output: {
+		filename: '[name].bundle.js',
+		chunkFilename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	}
 };
