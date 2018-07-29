54765f468eed0f9fce2b4a401cba9518b5bc6a70
shengliang74 <shengliang74@yeah.net>
2018-07-29 10:39:25 +0800

    webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import _ from 'lodash';
+import printMe from './print.js';
 
 function getComponent() {
-	return import(/* webpackChunkName: "lodash" */ 'lodash').then( _ => {
-		var element = document.createElement('div');
+	// return import(/* webpackChunkName: "lodash" */ 'lodash').then( _ => {
+		
+	// }).catch(error => 'An error occurred while loading the component')
+	var element = document.createElement('div');
 		element.innerHTML = _.join(['Hello','webpack',' ']);
 		return element;
-	}).catch(error => 'An error occurred while loading the component')
 }
 
-getComponent().then(component => {
-	document.body.appendChild(component);
-})
+function getBtn(){
+	var element = document.createElement('buttom');
+	element.innerHTML = _.join(['click','click2']);
+	return element
+}
+printMe();
+
+// getComponent().then(component => {
+// 	document.body.appendChild(component);
+// })
+document.body.appendChild(getComponent());
+document.body.appendChild(getBtn());
